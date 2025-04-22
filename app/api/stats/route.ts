a1ddc1f1cac7ae588e87b5614254e10102f68b33
wujunqi <996572550@qq.com>
2025-04-22 10:03:36 +0800

    feat: up

--- app/api/stats/route.ts
+++ app/api/stats/route.ts
@@ -1,22 +1,22 @@
 import type { NextRequest } from 'next/server'
-import { getBlogStats, updateBlogStats } from '~/db/queries'
-import type { SelectStats, StatsType } from '~/db/schema'
+// import { getBlogStats, updateBlogStats } from '~/db/queries'
+// import type { SelectStats, StatsType } from '~/db/schema'
 
 export async function GET(request: NextRequest) {
   try {
-    let { searchParams: params } = new URL(request.url)
-    let slug = params.get('slug')
-    let type = params.get('type') as StatsType
-    if (!slug || !type) {
-      return Response.json(
-        { message: 'Missing `type` or `slug` parameter!' },
-        {
-          status: 400,
-        }
-      )
-    }
-    let data = await getBlogStats(type, slug)
-    return Response.json(data)
+    // let { searchParams: params } = new URL(request.url)
+    // let slug = params.get('slug')
+    // let type = params.get('type') as StatsType
+    // if (!slug || !type) {
+    //   return Response.json(
+    //     { message: 'Missing `type` or `slug` parameter!' },
+    //     {
+    //       status: 400,
+    //     }
+    //   )
+    // }
+    // let data = await getBlogStats(type, slug)
+    return Response.json({ views: 0, likes: 0 })
   } catch (e) {
     console.error(e)
     return Response.json(
@@ -30,18 +30,18 @@ export async function GET(request: NextRequest) {
 
 export async function POST(request: NextRequest) {
   try {
-    let data: SelectStats = await request.json()
-    let { type, slug, ...updates } = data
-    if (!slug || !type) {
-      return Response.json(
-        { message: 'Missing `type` or `slug` parameter!' },
-        {
-          status: 400,
-        }
-      )
-    }
-    let updatedStats = await updateBlogStats(type, slug, updates)
-    return Response.json(updatedStats)
+    // let data: SelectStats = await request.json()
+    // let { type, slug, ...updates } = data
+    // if (!slug || !type) {
+    //   return Response.json(
+    //     { message: 'Missing `type` or `slug` parameter!' },
+    //     {
+    //       status: 400,
+    //     }
+    //   )
+    // }
+    // let updatedStats = await updateBlogStats(type, slug, updates)
+    return Response.json({ views: 0, likes: 0 })
   } catch (e) {
     console.error(e)
     return Response.json({ message: 'Internal Server Error!' }, { status: 500 })
